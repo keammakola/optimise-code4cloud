26732f2bf7c83b32982f06cc587f8a0ad2d9077c
Kea Mmakola <keammakola@gmail.com>
2025-08-19 12:50:57 +0200

    updated code logic

--- gdg_delivery.py
+++ gdg_delivery.py
@@ -7,18 +7,18 @@ def gdg_van(city, packages):
             if house == pkg:
                 delivered.append(pkg)
                 break
-    print("GDG van delivered", len(delivered), "packages!")
+    print("GDG vans delivered", len(delivered), "packages!")
     
 
 if __name__ == "__main__":
     city = []
-    for i in range(1_800_000):
+    for i in range(4_000_000):
         city.append(f"house_{i}")
     
         
     packages = []
-    for i in range(50000):
-        packages.append(f"house_{i % 100000}")
+    for i in range(80000):
+        packages.append(f"house_{i % 4_000_000}")
         
     # print(packages)
     
@@ -26,4 +26,8 @@ start = time.perf_counter()
 gdg_van( city,packages)
 end = time.perf_counter()
 print("GDG van took", round(end - start, 2), "seconds")
-        
+  
+  
+# account for all delivery vehicles in gauteng - 5000
+
+# also account for all possible addresses in sa -5 400 000   
